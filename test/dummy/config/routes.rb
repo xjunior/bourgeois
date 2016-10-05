435d19ce8a39d9f61409c1f6582475a71352e44b
Carlos Palhares <chjunior@gmail.com>
2016-10-05 00:11:33 -0300

    Add RailsAdmin as indirect dependency

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   mount Bourgeois::Engine => "/bourgeois"
+  mount RailsAdmin::Engine => "/admin"
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004033513) do
-
-  create_table "bourgeois_products", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "current_price"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
