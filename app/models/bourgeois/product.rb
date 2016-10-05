f38e4a3ba063f3a88e3716ccbe9d24d8e4ba46b1
Carlos Palhares <chjunior@gmail.com>
2016-10-05 00:57:03 -0300

    Display product information

--- app/models/bourgeois/product.rb
+++ app/models/bourgeois/product.rb
@@ -1,4 +1,7 @@
 module Bourgeois
   class Product < ApplicationRecord
+    def price_in_dollars
+      current_price / 100.0
+    end
   end
 end
--- test/models/bourgeois/product_test.rb
+++ test/models/bourgeois/product_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 module Bourgeois
   class ProductTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "price_in_dollars is current_price / 100" do
+      product = Bourgeois::Product.new(current_price: 1050)
+      assert_equal product.price_in_dollars, 10.5
+    end
   end
 end
