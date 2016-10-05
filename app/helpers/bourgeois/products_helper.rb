94d829bff88bb8a40db7c7efab8e06e9fc0f6c0d
Carlos Palhares <chjunior@gmail.com>
2016-10-05 15:20:26 -0300

    Add markdown long description to product

--- app/helpers/bourgeois/products_helper.rb
+++ app/helpers/bourgeois/products_helper.rb
@@ -1,7 +1,17 @@
+require 'redcarpet'
+
 module Bourgeois
   module ProductsHelper
     def product_price(product)
       number_to_currency product.price_in_dollars
+    end
+
+    def product_long_description(product)
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, {
+        autolink: true,
+        tables: true
+      })
+      markdown.render(product.long_description.to_s).html_safe
     end
   end
 end
--- test/helpers/products_helper_test.rb
+++ test/helpers/products_helper_test.rb
@@ -2,10 +2,23 @@ require 'test_helper'
 
 module Bourgeois
   class ProductsHelperTest < ActionView::TestCase
-    test "formats the price in dollars" do
-      product = Bourgeois::Product.new(current_price: 1100)
+    test 'formats the price in dollars' do
+      product = Bourgeois::Product.new current_price: 1100
 
       assert_equal product_price(product), '$11.00'
+    end
+
+    test 'formats the long description with markdown' do
+      product = Bourgeois::Product.new long_description: 'best **product** ever'
+      rendered = "<p>best <strong>product</strong> ever</p>\n"
+
+      assert_equal product_long_description(product), rendered
+    end
+
+    test 'formats the long description html safe' do
+      product = Bourgeois::Product.new long_description: 'lol'
+
+      assert product_long_description(product).html_safe?
     end
   end
 end
